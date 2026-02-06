25644beddfdfb22dc3bb657d5102867972b1ade2
Angelo <ace.blazado7@gmail.com>
2026-02-06 17:13:12 +0800

    update

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,23 @@
+require('dotenv').config();
 const app = require('./app');
 const logger = require('../config/logger');
+const connectDB = require('../config/database');
 
 const PORT = process.env.PORT || 3001;
 
+// Connect to database
+connectDB();
+
 app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
 
 process.on('uncaughtException', (err) => {
-    logger.fatal(err, 'Uncaught exception');
+  logger.fatal(err, 'Uncaught exception');
+  process.exit(1);
 });
 
 process.on('unhandledRejection', (err) => {
-    logger.fatal(err, 'UNhandled rejection');
-    process.exit(1);
+  logger.fatal(err, 'Unhandled rejection');
+  process.exit(1);
 });
