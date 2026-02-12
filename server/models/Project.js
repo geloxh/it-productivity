69abea30ca9ce5d5aaedca575a66318a4c2e46a8
geloxh <ace.blazado7@gmail.com>
2026-02-12 17:30:36 +0800

    db

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema ({
+    name: { type: String, required: true },
+    description: { type: String },
+    status: { type: String, enum: ['Planning', 'Active', 'Pending', 'Completed', 'Cancelled'], default: 'Planning'},
+    priority: { type: String, enum: ['Low', 'Medium', 'High', 'Critical'], default: 'Medium'},
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    members: 
+})
