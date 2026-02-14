da2b987f71b8d2f44a04b9564095931957791dfd
geloxh <ace.blazado7@gmail.com>
2026-02-14 19:37:50 +0800

    ticket routes

---
+++ server/routes/ticketRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { validate } = require('../middleware/validate');
+const { createTicketSchema, updateTicketSchema, addCommentSchema } = require('../validators/ticket.validator');
+
+router.post('/', validate(createTicketSchema), ticketController.creatge);
+router.put('/:id', validate(updateTicketSchema), ticketController.update);
+router.post('/:id/comments', validate(addCommentSchema), ticketController.addComment);
+
+module.exports = router;
