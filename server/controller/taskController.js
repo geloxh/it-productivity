a8c0a02cafb767bbe7547dde27cb6a69fcb15bea
geloxh <ace.blazado7@gmail.com>
2026-02-21 21:29:06 +0800

    task controller

---
+++ server/controller/taskController.js
@@ -0,0 +1,33 @@
+const Task = require('../models/Task');
+
+exports.create = async (req, res) => { 
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.getAll = async (req, res) => {
+    try {
+        const tasks = await Task.find().populate('project assignedTo');
+        res.json(tasks);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getById = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id).populate('projectAssignedto');
+        if (!task) return  res.status(404).json({ error: 'Task not found.' });
+        res.json(task);
+    } catch (error) {
+        res.json(500).json({ error: error.message });
+    }
+};
+
+/**
+ * update, delete functions
+ */
