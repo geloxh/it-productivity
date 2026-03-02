5aa83919b00ca41439ba5ac41ef6a661bdb8843a
geloxh <ace.blazado7@gmail.com>
2026-03-02 19:46:34 +0800

    missing getById route for projects

--- server/controller/projectController.js
+++ server/controller/projectController.js
@@ -18,6 +18,16 @@ exports.getAll = async (req, res) => {
     }
 };
 
+exports.getById = async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.id).populate('owner number');
+        if (!project) return res.status(404).json({ error: 'Project not found.'});
+        res.json(project);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
 exports.update = async (req, res) => {
     try {
         const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
