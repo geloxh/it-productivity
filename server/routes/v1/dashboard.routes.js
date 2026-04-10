b58479a65e0744ad9074a9d287cde9dbd03c468d
geloxh <ace.blazado7@gmail.com>
2026-04-10 09:44:04 +0800

    typo error

--- server/routes/v1/dashboard.routes.js
+++ server/routes/v1/dashboard.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();    
-const { getOverview, getAsssetMetrics, getTicketMetrics, getProjectMetrics, getTaskMetrics, getTimeSeries } = require('../../controller/dashboardController');
+const { getOverview, getAssetMetrics, getTicketMetrics, getProjectMetrics, getTaskMetrics, getTimeSeries } = require('../../controller/dashboardController');
 
 router.get('/overview', getOverview);
-router.get('/assets', getAssetMertrics);
+router.get('/assets', getAssetMetrics);
 router.get('/tickets', getTicketMetrics);
 router.get('/projects', getProjectMetrics);
 router.get('/tasks', getTaskMetrics);
