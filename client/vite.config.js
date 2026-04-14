4ec49ebbb6be71cd56cd90ae9665a71fdc3a9587
geloxh <ace.blazado7@gmail.com>
2026-04-14 13:06:16 +0800

    install shadcn ui and its components

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import path from 'path'
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+      tailwindcss()
+  ], resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       '/api': {
