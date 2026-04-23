d10c033070acf548f0460e5b79f8e171205a7717
geloxh <ace.blazado7@gmail.com>
2026-04-23 17:26:44 +0800

    update assets

--- server/models/Asset.js
+++ server/models/Asset.js
@@ -7,13 +7,74 @@ const assetSchema = new mongoose.Schema({
 
     category: {
         type: String,
-        enum: ['Laptop', 'Desktop', 'Server', 'Network', 'Peripheral', 'Software', 'Mobile'],
+        enum: [
+            'Laptop', 
+            'Desktop',
+            'AIO'
+        ],
         required: true
     },
-    manufacturer: { type: String },  
-    model: { type: String },
+    inclusions : {
+        type: String,
+        enum: [
+            'MOUSE',
+            'KEYBOARD',
+            'MOUSE PAD',
+            'LAPTOP CASE/BAG',
+            'FLASH DRIVE',
+            'CHARGER',
+            'POWER CORD',
+            'MONITOR'
+        ]
+    },
+    manufacturer: { // brand
+        type: String,
+        enum: [
+            'IPASON',
+            'ACER',
+            'RAZER',
+            'RAPOO',
+            'LOGITECH',
+            'LENOVO',
+            'BROTHER',
+            'DELL',
+            'CHICONY',
+            'INTEX',
+            'PROLINK',
+            'HUNTKEY',
+            'ASUS'
+        ]
+    },  
+    model: { 
+        type: String,
+        enum: [
+            'S14-72',
+            'EXTENSA S14-52',
+            'UNKNOWN',
+            'EXTENSA 214-53',
+            'SHANGQI A6470',
+            '82XQ',
+            '82XM',
+            'SHANGQI A24-1500',
+            'ASPIRE A314-35',
+            'ASPIRE A514-52K',
+            'VIVOBOOK -X1500EA',
+            'ASPIRE A315-510P',
+            'F0HN00KFCD',
+            '83K0'
+        ]
+    },
     deviceYearModel: { type: String },
-    systemInfo: { type: String },   
+    systemInfo: { 
+        type: String,
+        enum: [
+            'MICROSOFT WINDOWS SERVER',
+            'MICROSOFT WINDOWS 10 PRO',
+            'MICROSOFT WINDOWS 10 HOME SINGLE LANGUAGE',
+            'MICROSOFT WINDOWS 11 HOME SINGLE LANGUAGE ',
+            'MICROSOFT WINDOWS 11 PRO'
+        ]
+    },   
 
     status: {
         type: String,
@@ -22,19 +83,28 @@ const assetSchema = new mongoose.Schema({
     },
     equipmentStatus: {
         type: String,
-        enum: ['Good', 'Defective', 'For Repair', 'For Disposal'],
+        enum: [
+            'Excellent', 
+            'Good', 
+            'Fair', 
+            'Poor',
+            'Scrap'
+        ],
         default: 'Good'
     },
     contractStatus: {
         type: String,
-        enum: ['Active', 'Expired', 'None'],
+        enum: ['Active', 'Resigned', 'Awol', 'Terminated'],
         default: 'None'
     },
 
     assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
     user: { type: String },         
     formerUser: { type: String },
-    company: { type: String },
+    company: { 
+        type: String,
+        enum: ['SPK', '3E', 'PowerNet', 'NORM' ]
+    },
 
     dateAcquired: { type: Date },
     purchaseDate: { type: Date },
---
+++ server/models/NOTES.md
@@ -0,0 +1,15 @@
+### LEDGER DESCRIPTION :
+- **Excellent / Like New** : No visible scratches, dents, or damage.
+Works perfectly. Brand-new machine, no wear and tear, fully
+functional.
+-**Good** : Minor cosmetic wear (small scratches), fully functional.
+Slight marks on casing, keys slightly worn, no performance issues.
+-**Fair / Acceptable** : Noticeable wear or cosmetic damage, minor
+issues that do not prevent use. Scratched screen bezel, minor dents,
+battery not at full capacity.
+- **Poor / Damaged** : Major cosmetic damage or functional issues
+affecting usability.
+Broken keys, cracked screens, slow or failing hardware, require
+repair.
+- **Non-Functional / Scrap**: Cannot operate without major repairs.
+Dead motherboard, severe liquid damage, irreparable components.
