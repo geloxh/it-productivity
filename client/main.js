8c846b9a1277f7e8934aae9c997c7bdf8af8338c
geloxh <ace.blazado7@gmail.com>
2026-05-05 17:30:57 +0800

    add IPC + preload

--- client/main.js
+++ client/main.js
@@ -1,18 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const { spawn } = require('child_process')
 const path = require('path')
 
 let server
 
-app.whenReady().then(() => {
-    server = spawn('node', [path.join(__dirname, '../server/index.js')])
+function createMainWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
+  })
+  win.loadURL('http://localhost:5173')
+  return win
+}
 
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 800
-    })
-
-    win.loadURL('http://localhost:5000')
+ipcMain.handle('open-panel', (_, { route, width = 900, height = 650, title = '' }) => {
+  const panel = new BrowserWindow({
+    width, height, title,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
+  })
+  panel.loadURL(`http://localhost:5173${route}`)
 })
 
-app.on('quit', () => server.kill())
+app.whenReady().then(() => {
+  server = spawn('node', [path.join(__dirname, '../server/src/server.js')])
+  createMainWindow()
+})
+
+app.on('quit', () => server?.kill())
