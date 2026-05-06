0f3cd1a5865a112e8ef8b970b4cd8a0d5415512d
geloxh <ace.blazado7@gmail.com>
2026-05-06 16:51:50 +0800

    fix preload name

---
+++ client/main.cjs
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const { spawn } = require('child_process')
+const path = require('path')
+
+let server
+
+function createMainWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
+  })
+  win.loadURL('http://localhost:5173')
+  return win
+}
+
+ipcMain.handle('open-panel', (_, { route, width = 900, height = 650, title = '' }) => {
+  const panel = new BrowserWindow({
+    width, height, title,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
+  })
+  panel.loadURL(`http://localhost:5173${route}`)
+})
+
+app.whenReady().then(() => {
+  server = spawn('node', [path.join(__dirname, '../server/src/server.js')])
+  createMainWindow()
+})
+
+app.on('quit', () => server?.kill())
